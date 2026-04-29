541d00437b7126df0a29be94c79e9b2d86f85c56
Adi-3108 <uaditya763@gmail.com>
2026-04-29 11:00:22 +0530

    Deploy the backend changes to the production environment.

--- backend/start.sh
+++ backend/start.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 set -e
 
-python -m alembic upgrade head
+cd /app
+
+python -m alembic -c /app/alembic.ini upgrade head
 exec python -m uvicorn app.main:app --host 0.0.0.0 --port "${PORT:-8000}"
